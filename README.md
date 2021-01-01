8d6bc29dc3c9146193d451f2c518d0a4894f49ac
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2021-01-01 19:13:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.js-and-Express-Portfolio
+ Static site to present some of my JavaScript Projects using Node and Express
---
+++ app.js
@@ -0,0 +1,52 @@
+/**********************************************************************************
+*                                                                                 *                      
+*                    Static Node.js and Express - Portfolio                       *
+*                                                                                 *
+*                                 Matt Hawes                                      *
+*                                                                                 *
+/**********************************************************************************/
+//NECESSARY DEPENDENCIES
+const express = require('express');
+const data = require('./data.json').projects;
+
+//MODULE APPLICATIONS
+const app = express();
+
+//SET VIEW ENGINE TO PUG
+app.set('view engine', 'pug');
+
+//STATIC ROUTE TO SERVE STATIC FILES LOCATED IN PUBLIC FOLDER - (CSS AND JS)
+app.use('/static', express.static('public'));
+
+//******** MY ROUTES *********//
+//HOME PAGE
+app.get('/', (req,res) => {
+	res.render('index', { data })
+});
+
+//ABOUT PAGE
+app.get('/about', (req,res) => {
+	res.render('about')
+});
+
+//PROJECT PAGES
+data.map(project => app.get(`/projects/${project.id}`, (req, res) => {
+	  res.render('project', { project })
+	})
+);
+
+//******* ERROR HANDLERS *******//
+app.use((req,res,next) => {
+	const err = new Error("I'm sorry... This page does not exist!")
+	err.status = 404;
+	next(err)
+});
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(err.status)
+	res.render('error');
+});
+
+//DEV SERVER TO RUN ON LOCAL HOST (PORT:3000)
+app.listen(3000);
